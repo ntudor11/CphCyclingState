470b02aa21382b9a91af138eb755dff7d921fc60
Tudor Nastasa <tudorrr.nastasa@gmail.com>
2020-10-10 18:20:40 +0200

    dynamic charts for multiple weather params

--- src/components/WeatherStats.jsx
+++ src/components/WeatherStats.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import {
   LineChart,
   Line,
@@ -7,13 +7,16 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend
+  Legend,
+  ResponsiveContainer
 } from "recharts";
 
 const WeatherStats = props => {
   const [state, setState] = useState({});
 
-  const { lat, long } = props;
+  // const { lat, long } = props;
+
+  const [lat, long] = [55.764184, 12.199226];
 
   useEffect(() => {
     try {
@@ -27,47 +30,65 @@ const WeatherStats = props => {
     }
   }, [lat, long]);
 
-  let data = [];
+  let temperatureData = [];
+  let rainData = [];
 
   state.properties &&
     state.properties.timeseries.forEach(point => {
-      const dataPoint = {
+      const tempDataPoint = {
         name: new Date(point.time).toLocaleString(),
         temperature: point.data.instant.details.air_temperature
       };
-      data.push(dataPoint);
+      const rainDataPoint = {
+        name: new Date(point.time).toLocaleString(),
+        probability_of_precipitation:
+          point.data.next_1_hours &&
+          point.data.next_1_hours.details.probability_of_precipitation
+      };
+      temperatureData.push(tempDataPoint);
+      rainData.push(rainDataPoint);
     });
 
-  return (
-    <Container>
-      <h3>
-        Weather Forecast for coordinates: {lat} x {long}
-      </h3>
+  const data = [temperatureData, rainData];
 
-      <LineChart
-        width={800}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="temperature"
-          stroke="#82ca9d"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </Container>
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <h3>
+          Weather Forecast for coordinates: {lat} x {long}
+        </h3>
+      </Grid>
+
+      {data.map(dataSet => (
+        <Grid item xs={12} sm={6}>
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart
+              width={500}
+              height={300}
+              data={dataSet}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey={dataSet.map(inDataSet => Object.keys(inDataSet)[1])[0]}
+                stroke="#16a5b9"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
